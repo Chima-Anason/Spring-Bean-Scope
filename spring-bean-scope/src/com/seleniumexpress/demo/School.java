9ee1b899125087509293e02438219a7e898dce9a
Chima-Anason <tblynxx@gmail.com>
2022-06-22 02:07:05 +0800

    Second approach for solving Bean Scope injection problem by using @Lookup method

--- spring-bean-scope/src/com/seleniumexpress/demo/School.java
+++ spring-bean-scope/src/com/seleniumexpress/demo/School.java
@@ -1,12 +1,13 @@
 package com.seleniumexpress.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 @Scope("singleton")
-public class School {
+public abstract class School {
 	
 	@Autowired
 	private Student student;
@@ -14,8 +15,15 @@ public class School {
 	public School() {
 		System.out.println("School obj created.....");
 	}
+	
+	
+	@Lookup
+	abstract Student createStudentObj();
 
 	public Student getStudent() {
+		
+		Student student =createStudentObj();
+		
 		return student;
 	}
 
